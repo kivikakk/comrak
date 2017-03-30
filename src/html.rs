7bb3ecc8602eec2b35defc77759606737e993035
Yuki Izumi <yuki@kivikakk.ee>
2017-03-30 20:06:09 +1100

    I accidentally a whole tests pass

--- src/html.rs
+++ src/html.rs
@@ -8,6 +8,11 @@ pub fn format_document<'a>(root: &'a Node<'a, AstCell>) -> String {
     String::from_utf8(res).unwrap()
 }
 
+struct HtmlFormatter<'a> {
+    v: Vec<u8>,
+    w: &'a mut Write,
+}
+
 fn format_node<'a>(w: &mut Write, node: &'a Node<'a, AstCell>) {
     match &node.data.borrow().value {
         &NodeValue::Document => {
@@ -16,13 +21,16 @@ fn format_node<'a>(w: &mut Write, node: &'a Node<'a, AstCell>) {
             }
         }
         &NodeValue::BlockQuote => {
+            cr();
             write!(w, "<blockquote>\n").unwrap();
             for n in node.children() {
                 format_node(w, n);
             }
+            cr();
             write!(w, "</blockquote>\n").unwrap()
         }
         &NodeValue::List(ref nl) => {
+            cr();
             if nl.list_type == ListType::Bullet {
                 write!(w, "<ul>\n").unwrap();
             } else if nl.start == 1 {
@@ -42,13 +50,15 @@ fn format_node<'a>(w: &mut Write, node: &'a Node<'a, AstCell>) {
             }
         }
         &NodeValue::Item(..) => {
+            cr();
             write!(w, "<li>");
             for n in node.children() {
                 format_node(w, n);
             }
-            write!(w, "</li>");
+            write!(w, "</li>\n");
         }
         &NodeValue::Heading(ref nch) => {
+            cr();
             write!(w, "<h{}>", nch.level).unwrap();
             for n in node.children() {
                 format_node(w, n);
@@ -56,6 +66,7 @@ fn format_node<'a>(w: &mut Write, node: &'a Node<'a, AstCell>) {
             write!(w, "</h{}>\n", nch.level).unwrap();
         }
         &NodeValue::CodeBlock(ref ncb) => {
+            cr();
             write!(w, "<pre><code").unwrap();
             if ncb.info != "" {
                 write!(w, " class=\"language-{}\"", ncb.info).unwrap();
@@ -78,12 +89,21 @@ fn format_node<'a>(w: &mut Write, node: &'a Node<'a, AstCell>) {
             // TODO
         }
         &NodeValue::Paragraph => {
-            // TODO: tight list setting
-            write!(w, "<p>").unwrap();
+            let tight = match node.parent().and_then(|ref n| n.parent()).map(|ref n| n.data.borrow().value.clone()) {
+                Some(NodeValue::List(ref nl)) => nl.tight,
+                _ => false,
+            };
+
+            if !tight {
+                cr();
+                write!(w, "<p>").unwrap();
+            }
             for n in node.children() {
                 format_node(w, n);
             }
-            write!(w, "</p>\n").unwrap();
+            if !tight {
+                write!(w, "</p>\n").unwrap();
+            }
         }
         &NodeValue::Text(ref literal) => {
             // TODO: escape HTML
@@ -132,3 +152,5 @@ fn format_node<'a>(w: &mut Write, node: &'a Node<'a, AstCell>) {
         }
     }
 }
+
+fn cr() {}
