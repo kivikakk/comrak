f85ca7d64ec776fcdda0f818b743277869115ad4
Ashe Connor <ashe@kivikakk.ee>
2018-04-13 18:59:56 +1000

    replace char vecs with arrays
    Signed-off-by: Ashe Connor <ashe@kivikakk.ee>

--- src/parser/inlines.rs
+++ src/parser/inlines.rs
@@ -26,8 +26,8 @@ pub struct Subject<'a: 'd, 'r, 'o, 'd, 'i> {
     brackets: Vec<Bracket<'a, 'd>>,
     pub backticks: [usize; MAXBACKTICKS + 1],
     pub scanned_for_backticks: bool,
-    special_chars: Vec<bool>,
-    smart_chars: Vec<bool>,
+    special_chars: [bool; 256],
+    smart_chars: [bool; 256],
 }
 
 pub struct Delimiter<'a: 'd, 'd> {
@@ -68,10 +68,9 @@ impl<'a, 'r, 'o, 'd, 'i> Subject<'a, 'r, 'o, 'd, 'i> {
             brackets: vec![],
             backticks: [0; MAXBACKTICKS + 1],
             scanned_for_backticks: false,
-            special_chars: vec![],
-            smart_chars: vec![],
+            special_chars: [false; 256],
+            smart_chars: [false; 256],
         };
-        s.special_chars.extend_from_slice(&[false; 256]);
         for &c in &[
             b'\n', b'\r', b'_', b'*', b'"', b'`', b'\\', b'&', b'<', b'[', b']', b'!'
         ] {
@@ -83,7 +82,6 @@ impl<'a, 'r, 'o, 'd, 'i> Subject<'a, 'r, 'o, 'd, 'i> {
         if options.ext_superscript {
             s.special_chars[b'^' as usize] = true;
         }
-        s.smart_chars.extend_from_slice(&[false; 256]);
         for &c in &[
             b'"', b'\'', b'.', b'-',
         ] {
