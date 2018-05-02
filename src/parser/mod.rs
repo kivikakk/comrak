b28c7a147cebac0790e0125dfefdfbc435a1db20
Brian Anderson <andersrb@gmail.com>
2018-05-01 17:04:04 -0700

    Convert Parser::postprocess_text_nodes from recursion to iteration
    Note that this changes the traversal order slightly, processing
    all siblings before descending into children. This could affect
    cache behavior in subsequent passes, but looking at the benchmark
    I haven't found anything conclusive.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1273,45 +1273,54 @@ impl<'a, 'o> Parser<'a, 'o> {
     }
 
     fn postprocess_text_nodes(&mut self, node: &'a AstNode<'a>) {
-        let mut nch = node.first_child();
+        let mut stack = vec![node];
+        let mut children = vec![];
 
-        while let Some(n) = nch {
-            let mut this_bracket = false;
-            loop {
-                match n.data.borrow_mut().value {
-                    NodeValue::Text(ref mut root) => {
-                        let ns = match n.next_sibling() {
-                            Some(ns) => ns,
-                            _ => {
-                                self.postprocess_text_node(n, root);
-                                break;
-                            }
-                        };
+        while let Some(node) = stack.pop() {
+            let mut nch = node.first_child();
 
-                        match ns.data.borrow().value {
-                            NodeValue::Text(ref adj) => {
-                                root.extend_from_slice(adj);
-                                ns.detach();
-                            }
-                            _ => {
-                                self.postprocess_text_node(n, root);
-                                break;
+            while let Some(n) = nch {
+                let mut this_bracket = false;
+                loop {
+                    match n.data.borrow_mut().value {
+                        NodeValue::Text(ref mut root) => {
+                            let ns = match n.next_sibling() {
+                                Some(ns) => ns,
+                                _ => {
+                                    self.postprocess_text_node(n, root);
+                                    break;
+                                }
+                            };
+
+                            match ns.data.borrow().value {
+                                NodeValue::Text(ref adj) => {
+                                    root.extend_from_slice(adj);
+                                    ns.detach();
+                                }
+                                _ => {
+                                    self.postprocess_text_node(n, root);
+                                    break;
+                                }
                             }
                         }
+                        NodeValue::Link(..) | NodeValue::Image(..) => {
+                            this_bracket = true;
+                            break;
+                        }
+                        _ => break,
                     }
-                    NodeValue::Link(..) | NodeValue::Image(..) => {
-                        this_bracket = true;
-                        break;
-                    }
-                    _ => break,
                 }
+
+                if !this_bracket {
+                    children.push(n);
+                }
+
+                nch = n.next_sibling();
             }
 
-            if !this_bracket {
-                self.postprocess_text_nodes(n);
-            }
-
-            nch = n.next_sibling();
+            // Push children onto work stack in reverse order so they are
+            // traversed in order
+            stack.extend(children.drain(..).rev());
         }
     }
 
