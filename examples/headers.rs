3b2f2fc0a527ab9946cc078ac027819bcac34918
Asherah Connor <ashe@kivikakk.ee>
2021-03-23 09:46:18 +1100

    examples: add "headers"

---
+++ examples/headers.rs
@@ -0,0 +1,48 @@
+extern crate comrak;
+
+use comrak::{Arena, parse_document, ComrakOptions, nodes::{AstNode, NodeValue}};
+
+fn main() {
+    println!("{:?}", get_document_title("# Hello\n"));
+    println!("{:?}", get_document_title("## Hello\n"));
+    println!("{:?}", get_document_title("# `hi` **there**\n"));
+}
+
+fn get_document_title(document: &str) -> String {
+    let arena = Arena::new();
+    let root = parse_document(&arena, document, &ComrakOptions::default());
+
+    for node in root.children() {
+        let header = match node.data.clone().into_inner().value {
+            NodeValue::Heading(c) => c,
+            _ => continue,
+        };
+
+        if header.level != 1 {
+            continue;
+        }
+
+        let mut text = Vec::new();
+        collect_text(node, &mut text);
+
+        // The input was already known good UTF-8 (document: &str) so comrak
+        // guarantees the output will be too.
+        return String::from_utf8(text).unwrap();
+    }
+
+    "Untitled Document".to_string()
+}
+
+fn collect_text<'a>(node: &'a AstNode<'a>, output: &mut Vec<u8>) {
+    match node.data.borrow().value {
+        NodeValue::Text(ref literal) | NodeValue::Code(ref literal) => {
+            output.extend_from_slice(literal)
+        }
+        NodeValue::LineBreak | NodeValue::SoftBreak => output.push(b' '),
+        _ => {
+            for n in node.children() {
+                collect_text(n, output);
+            }
+        }
+    }
+}
