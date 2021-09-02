1aed38045fe0681e28fc64f1b18d835c4931c184
Ashe Connor <ashe@kivikakk.ee>
2021-09-02 17:37:45 +1000

    release-raven: getting the gnu toolchain working too hard

--- script/release-raven.ps1
+++ script/release-raven.ps1
@@ -6,6 +6,7 @@ echo $version
 cargo +stable build --release
 mv target/release/comrak.exe comrak-$version-x86_64-pc-windows-msvc.exe
 
-rustup +stable toolchain install stable-x86_64-pc-windows-gnu
-rustup run stable-gnu cargo build --release --target=x86_64-pc-windows-gnu
-mv target/x86_64-pc-windows-gnu/release/comrak.exe comrak-$version-x86_64-pc-windows-gnu.exe
+# rustup +stable toolchain install stable-x86_64-pc-windows-gnu
+# $env:Path += ";C:\msys64\usr\bin"
+# rustup run stable-gnu cargo build --release --target=x86_64-pc-windows-gnu
+# mv target/x86_64-pc-windows-gnu/release/comrak.exe comrak-$version-x86_64-pc-windows-gnu.exe
