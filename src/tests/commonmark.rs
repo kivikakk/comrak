78123f5bb5fbfa1d9a780d12c924c333b98d70cf
jnnnnn <jnnnnn@gmail.com>
2023-07-17 13:54:06 +1200

    new test wrap preserve link

--- src/tests/commonmark.rs
+++ src/tests/commonmark.rs
@@ -9,3 +9,18 @@ fn commonmark_removes_redundant_strong() {
 
     commonmark(input, output, Some(&options));
 }
+
+#[test]
+fn commonmark_wrap_preserve_link() {
+    let options = ComrakOptions {
+        render: ComrakRenderOptions {
+            width: 20,
+            ..ComrakRenderOptions::default()
+        },
+        ..Default::default()
+    };
+
+    let input = "This is [a link containing spaces that should not be wrapped](https://example.com) and then the text continues after.";
+    let output = "This is\n[a link containing spaces that should not be wrapped](https://example.com)\nand then the text\ncontinues after.\n";
+    commonmark(input, output, Some(&options));
+}
