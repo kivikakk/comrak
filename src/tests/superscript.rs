1c469a7829b5cdc12c811a345355c6d0d6816810
Asherah Connor <ashe@kivikakk.ee>
2025-09-11 11:47:29 +1000

    superscript: failing test for #592.

--- src/tests/superscript.rs
+++ src/tests/superscript.rs
@@ -8,3 +8,12 @@ fn superscript() {
         concat!("<p>e = mc<sup>2</sup>.</p>\n"),
     );
 }
+
+#[test]
+fn superscript_negative_exponents_are_real() {
+    html_opts!(
+        [extension.superscript],
+        concat!("i^-2^ = -1.\n"),
+        concat!("<p>i<sup>-2</sup> = -1.</p>\n"),
+    );
+}
