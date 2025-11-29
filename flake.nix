9c78e3d504f422c4ce13e5332259e61ecb1e5834
Asherah Connor <ashe@kivikakk.ee>
2025-11-29 16:48:49 +1100

    flake.nix: nixos-25.11.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "comrak";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     fenix = {
       url = "github:nix-community/fenix";
