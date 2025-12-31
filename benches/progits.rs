57ed6748f8607ebbc4fd1074bb16c9f1609c9a86
Asherah Connor <ashe@kivikakk.ee>
2025-12-31 18:36:34 +1100

    Reproduce "fuller" benchmark for CodSpeed

--- benches/progits.rs
+++ benches/progits.rs
@@ -4,19 +4,23 @@ extern crate test;
 
 use comrak::{format_html, parse_document, Arena, Options};
 use divan::Bencher;
+use glob::glob;
+use std::fs::File;
+use std::io::{BufReader, Read};
 
 fn main() {
     divan::main();
 }
 
 #[divan::bench]
-fn bench_progit(b: Bencher) {
-    use std::fs::File;
-    use std::io::Read;
+fn bench_progits(b: Bencher) {
+    let mut s = String::with_capacity(12_000_000);
+    for entry in glob("vendor/progit/*/*/*.markdown").unwrap() {
+        let file = File::open(entry.unwrap()).unwrap();
+        let mut br = BufReader::new(file);
+        br.read_to_string(&mut s).unwrap();
+    }
 
-    let mut file = File::open("script/progit.md").unwrap();
-    let mut s = String::with_capacity(524288);
-    file.read_to_string(&mut s).unwrap();
     b.bench(|| {
         let arena = Arena::new();
         let root = parse_document(&arena, &s, &Options::default());
