113ba2d1bcee6edee0bd02352c0f126627b91aee
Asherah Connor <ashe@kivikakk.ee>
2026-03-10 22:38:23 +1100

    Change pathological timeout back to 4s

--- src/tests/pathological.rs
+++ src/tests/pathological.rs
@@ -5,7 +5,7 @@ use ntest::timeout;
 
 // input: python3 -c 'n = 50000; print("*a_ " * n)'
 #[test]
-#[timeout(8000)]
+#[timeout(4000)]
 fn pathological_emphases() {
     let n = 50_000;
     let input = "*a_ ".repeat(n);
