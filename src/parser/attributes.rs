36374ae03363bd1aab06266686729c760652cc1f
Asherah Connor <ashe@kivikakk.ee>
2026-07-04 16:41:28 +1000

    attributes: complete parser!

--- src/parser/attributes.rs
+++ src/parser/attributes.rs
@@ -10,13 +10,23 @@ pub fn parse_attributes(input: &str) -> Option<(Attributes, usize)> {
 
     enum State {
         Betwixt,
-        Value(Kind, String),
+        Value(Kind, String, Quote),
+        Key(String),
+        PostQuote,
     }
 
+    #[derive(Default)]
     enum Kind {
+        #[default]
         Id,
         Class,
         Pair(String),
+    }
+
+    enum Quote {
+        Bare,
+        Quoted,
+        QuotedEscaped,
     }
 
     let mut state = State::Betwixt;
@@ -29,17 +39,26 @@ pub fn parse_attributes(input: &str) -> Option<(Attributes, usize)> {
             match state {
                 State::Betwixt => match c {
                     '#' => {
-                        state = State::Value(Kind::Id, String::new());
+                        state = State::Value(Kind::Id, String::new(), Quote::Bare);
                         break;
                     }
                     '.' => {
-                        state = State::Value(Kind::Class, String::new());
+                        state = State::Value(Kind::Class, String::new(), Quote::Bare);
+                        break;
+                    }
+                    'a'..='z' | 'A'..='Z' => {
+                        state = State::Key(c.to_string());
                         break;
                     }
                     '}' => return Some((attrs, i + 1)),
-                    _ => todo!(),
+                    ' ' | '\t' => break,
+                    _ => return None,
                 },
-                State::Value(ref kind, ref mut value) => match c {
+                State::Value(ref mut kind, ref mut value, Quote::Bare) => match c {
+                    '"' if value.is_empty() => {
+                        state = State::Value(mem::take(kind), mem::take(value), Quote::Quoted);
+                        break;
+                    }
                     // An id can contain [literally anything] in HTML5, just so long as it's non-empty.
                     // Consider looking at how Pandoc parses this part.
                     // Per above, classes are also literally anything, *except* ASCII whitespace.
@@ -51,14 +70,74 @@ pub fn parse_attributes(input: &str) -> Option<(Attributes, usize)> {
                     }
                     ' ' | '}' => {
                         match kind {
-                            Kind::Id => attrs.id = Some(mem::take(value)),
-                            Kind::Class => attrs.classes.push(mem::take(value)),
-                            _ => todo!(),
+                            Kind::Id => {
+                                if value.is_empty() {
+                                    return None;
+                                }
+                                attrs.id = Some(mem::take(value));
+                            }
+                            Kind::Class => {
+                                if value.is_empty() {
+                                    return None;
+                                }
+                                attrs.classes.push(mem::take(value));
+                            }
+                            Kind::Pair(key) => attrs.pairs.push((mem::take(key), mem::take(value))),
                         }
                         state = State::Betwixt;
                         // handle in loop
                     }
                     _ => todo!(),
+                },
+                State::Value(ref mut kind, ref mut value, Quote::Quoted) => match c {
+                    '"' => {
+                        // XXX: duplicates above, wish it didn't.
+                        match kind {
+                            Kind::Id => attrs.id = Some(mem::take(value)),
+                            Kind::Class => attrs.classes.push(mem::take(value)),
+                            Kind::Pair(key) => attrs.pairs.push((mem::take(key), mem::take(value))),
+                        }
+                        state = State::PostQuote;
+                        break;
+                    }
+                    '\\' => {
+                        state =
+                            State::Value(mem::take(kind), mem::take(value), Quote::QuotedEscaped);
+                        break;
+                    }
+                    _ => {
+                        value.push(c);
+                        break;
+                    }
+                },
+                State::Value(ref mut kind, ref mut value, Quote::QuotedEscaped) => {
+                    value.push(c);
+                    state = State::Value(mem::take(kind), mem::take(value), Quote::Quoted);
+                    break;
+                }
+                State::Key(ref mut key) => match c {
+                    // "Except where otherwise specified, attribute values on HTML elements may be any string value,
+                    // including the empty string, and there is no restriction on what text can be specified in such
+                    // attribute values."
+                    // ¯\_(ツ)_/¯ I'm not driving
+                    'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_' | ':' | '.' => {
+                        key.push(c);
+                        break;
+                    }
+                    '=' => {
+                        state =
+                            State::Value(Kind::Pair(mem::take(key)), String::new(), Quote::Bare);
+                        break;
+                    }
+                    _ => return None,
+                },
+                State::PostQuote => match c {
+                    // Require a space after quote before anything else.
+                    ' ' | '}' => {
+                        state = State::Betwixt;
+                        // handle in loop
+                    }
+                    _ => return None,
                 },
             }
         }
@@ -71,7 +150,7 @@ mod tests {
 
     #[test]
     fn trivial() {
-        assert!(parse_attributes("").is_none());
+        assert_eq!(parse_attributes(""), None);
 
         assert_eq!(
             parse_attributes("{#henlo} there"),
@@ -96,6 +175,17 @@ mod tests {
         );
 
         assert_eq!(
+            parse_attributes("{data-thingy=ya}"),
+            Some((
+                Attributes {
+                    pairs: vec![("data-thingy".to_string(), "ya".to_string())],
+                    ..Default::default()
+                },
+                16
+            ))
+        );
+
+        assert_eq!(
             parse_attributes("{.oken #yip m=26 .sure x=3 #yap} oh!"),
             Some((
                 Attributes {
@@ -106,28 +196,43 @@ mod tests {
                         ("x".to_string(), "3".to_string())
                     ]
                 },
-                7 // XXX
+                32
             ))
         );
 
         assert_eq!(
             parse_attributes(
-                "{#\"has space, will travel\" .\"ok\\\"en\" title=\"surely \\\"not\\\"\"}"
+                "{#\"has space, will travel\" .\"ok\\\"en\" title=\"是非 \\\"not\\\"\"}"
             ),
             Some((
                 Attributes {
                     id: Some("has space, will travel".to_string()),
                     classes: vec!["ok\"en".to_string(),],
-                    pairs: vec![("title".to_string(), "surely \"not\"".to_string())],
+                    pairs: vec![("title".to_string(), "是非 \"not\"".to_string())],
                 },
-                7 // XXX
+                60
             ))
         );
 
-        assert!(parse_attributes("{#}").is_none());
-        assert_eq!(parse_attributes("{}"), Some(Default::default()));
-        assert!(parse_attributes("{.}").is_none());
-        assert!(parse_attributes("{uh").is_none());
-        assert!(parse_attributes("{yeah\nnah}").is_none());
+        assert_eq!(parse_attributes("{#}"), None);
+        assert_eq!(parse_attributes("{}"), Some((Attributes::default(), 2)));
+        assert_eq!(parse_attributes("{.}"), None);
+        assert_eq!(parse_attributes("{uh"), None);
+        assert_eq!(parse_attributes("{yeah\nnah}"), None);
+
+        assert_eq!(
+            parse_attributes("{hi=}"),
+            Some((
+                Attributes {
+                    pairs: vec![("hi".to_string(), String::new())],
+                    ..Default::default()
+                },
+                5
+            ))
+        );
+
+        // XXX: I kind of feel like this should be equivalent to above.
+        // Check Pandoc.
+        assert_eq!(parse_attributes("{hi}"), None);
     }
 }
