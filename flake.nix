c02e9afb3ededcc96ee22118c2f7cf1b4ce3ac0b
Asherah Connor <ashe@kivikakk.ee>
2026-09-10 10:47:02 +1000

    iqan tarball.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "comrak";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.zst";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
